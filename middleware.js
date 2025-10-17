4bd985a41adddaa62fb90349a14752ded4903b7c
prajwalrankhamb <prajwalrankhamb7@gmail.com>
2025-10-17 21:39:13 +0530

    retrive chabges

--- middleware.js
+++ middleware.js
@@ -1,4 +1,4 @@
-import { detectBot, shield } from "@arcjet/next";
+import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
@@ -18,9 +18,13 @@ const aj = arcjet({
       mode: "LIVE",
     }),
     detectBot({
-      mode: "DRY_RUN", // logs instead of blocking
-      allow: ["CATEGORY:SEARCH_ENGINE", "GO_HTTP"],
-    })
+      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+        "GO_HTTP", // For Inngest
+        // See the full list at https://arcjet.com/bot-list
+      ],
+    }),
   ],
 });
 
