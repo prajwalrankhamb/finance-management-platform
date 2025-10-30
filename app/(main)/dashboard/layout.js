d1f51cd0d7db881d2238947905eaef05c3b196ab
prajwalrankhamb <prajwalrankhamb7@gmail.com>
2025-10-30 15:29:11 +0530

    Updated styles and theme colors

---
+++ app/(main)/dashboard/layout.js
@@ -0,0 +1,22 @@
+import React, { Suspense } from 'react'
+import DashboardPage from './page'
+import {BarLoader} from 'react-spinners'
+
+function DashboardLayout () {
+  return (
+    <div className='px-5'>
+      <h1 className='text-6xl font-bold mb-5'>
+        Dashboard
+      </h1>
+
+      {/* {Dashboard Page} */}
+      <Suspense 
+        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />} 
+      >
+            <DashboardPage />
+      </Suspense>
+    </div>
+ )
+}
+
+export default DashboardLayout;
---
+++ app/layout.js
@@ -0,0 +1,40 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from 'sonner';
+
+
+const inter = Inter({subsets: ["latin"]})
+
+export const metadata = {
+  title: "FinLytic",
+  description: "one step finance platform",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body
+        className={`${inter.className}`}
+      >
+        {/* {Header} */}
+        <Header/>
+      
+
+        <main className="min-h-screen">{children}</main>
+        <Toaster richColors/>
+        {/* {Footer} */}
+      <footer className="bg-blue-50 py-12">
+        <div className="container mx-auto px-4 text-center text-grey-600">
+          <p>
+            Made by our team
+          </p>
+        </div>
+      </footer>
+      </body>
+    </html>
+    </ClerkProvider>
+  );
+}
